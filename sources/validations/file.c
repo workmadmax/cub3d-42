d21b4b950b31a150f00e6794f63c3322711fbb69
workmadmax <work.madmax@gmail.com>
2023-07-14 17:51:16 -0300

    fix validate arguments and init flags for textures

--- sources/validations/file.c
+++ sources/validations/file.c
@@ -1,43 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   arguments.c                                        :+:      :+:    :+:   */
+/*   file.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/13 16:16:28 by madmax42          #+#    #+#             */
-/*   Updated: 2023/07/14 17:38:28 by madmax42         ###   ########.fr       */
+/*   Created: 2023/07/14 17:38:31 by madmax42          #+#    #+#             */
+/*   Updated: 2023/07/14 17:38:38 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	check_amount_arguments(int argc)
+int	check_file_extension(char *file)
 {
-	if (argc != 2)
+	int		len;
+
+	len = ft_strlen(file);
+	if (len < 5 || ft_strncmp(file + len - 4, ".cub", 4))
 	{
-		printf("Error no número de argumentos...\n");
+		printf("Error na extensão do arquivo...\n");
 		return (FAILURE);
 	}
 	return (SUCCESS);
 }
 
-int	check_arguments(int argc, char **argv)
+int	check_file_exists(const char *file)
 {
-	if (check_amount_arguments(argc) == FAILURE)
+	int		fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
 	{
-		exit(0);
+		printf("Error ao abrir o arquivo...\n");
 		return (FAILURE);
 	}
-	if (check_file_extension(argv[1]) == FAILURE)
-	{
-		exit(0);
-		return (FAILURE);
-	}
-	if (check_file_exists(argv[1]) == FAILURE)
-	{
-		exit(0);
-		return (FAILURE);
-	}
+	close(fd);
 	return (SUCCESS);
 }
