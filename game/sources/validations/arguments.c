56f64509647b93629c2d5d8a35fcf9c6cee3d3dd
workmadmax <work.madmax@gmail.com>
2023-08-02 11:35:25 -0300

    fix code args t_bool

--- game/sources/validations/arguments.c
+++ game/sources/validations/arguments.c
@@ -6,38 +6,35 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:16:28 by madmax42          #+#    #+#             */
-/*   Updated: 2023/07/14 17:38:28 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/08/02 11:34:35 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	check_amount_arguments(int argc)
+t_bool	check_amount_arguments(int argc)
 {
 	if (argc != 2)
-	{
-		printf("Error no número de argumentos...\n");
-		return (FAILURE);
-	}
-	return (SUCCESS);
+		return (FALSE);
+	return (TRUE);
 }
 
-int	check_arguments(int argc, char **argv)
+t_bool	check_arguments(int argc, char **argv)
 {
-	if (check_amount_arguments(argc) == FAILURE)
+	if (!check_amount_arguments(argc))
 	{
-		exit(0);
-		return (FAILURE);
+		exit (error_msg("	Error\nWrong amount of arguments\n", ERROR_MSG));
+		return (FALSE);
 	}
-	if (check_file_extension(argv[1]) == FAILURE)
+	if (check_file_extension(argv[1]) == FALSE)
 	{
-		exit(0);
-		return (FAILURE);
+		exit (error_msg("	Error\nWrong file extension\n", ERROR_EXTENSION));
+		return (FALSE);
 	}
-	if (check_file_exists(argv[1]) == FAILURE)
+	if (check_file_exists(argv[1]) == FALSE)
 	{
-		exit(0);
-		return (FAILURE);
+		exit (error_msg("	Error\nFile doesn't exist\n", ERROR_FILE));
+		return (FALSE);
 	}
-	return (SUCCESS);
+	return (TRUE);
 }
