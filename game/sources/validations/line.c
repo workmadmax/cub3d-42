fb3a66ee260c1325104995e8a90955f4754d78f3
workmadmax <work.madmax@gmail.com>
2023-08-02 21:17:25 -0300

    fix line.c

--- game/sources/validations/line.c
+++ game/sources/validations/line.c
@@ -6,30 +6,30 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 12:34:06 by madmax42          #+#    #+#             */
-/*   Updated: 2023/07/19 11:19:37 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/08/02 20:34:00 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	check_first_word(char **line)
+t_bool	check_first_word(char **line)
 {
 	int	i;
 
 	i = 0;
 	while (line[i])
 	{
-		if (get_line_code(line[i]) == FAILURE)
+		if (get_line_code(line[i]) == FALSE)
 		{
-			error_msg(line[i], ERROR_LINE);
-			return (FAILURE);
+			error_msg("Error\n**line invalid", ERROR_LINE);
+			return (FALSE);
 		}
 		i++;
 	}
-	return (SUCCESS);
+	return (TRUE);
 }
 
-int	check_line_position(char **line)
+t_bool	check_line_position(char **line)
 {
 	int	i;
 	int	map;
@@ -45,7 +45,7 @@ int	check_line_position(char **line)
 		if (map == 1 && status != WALL)
 			map = 2;
 		if (map == 2 && status != NEW_LINE && status != END)
-			return (error_msg(line[i], ERROR_LINE_WRONG_POSITION));
+			return (error_msg(line[i], ERROR_LINE));
 	}
-	return (SUCCESS);
+	return (TRUE);
 }
